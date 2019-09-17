bbfe660b9328c5757a86bd14ba450c923950bef3
Sathler <Vinicius.sathler3@yahoo.com.br>
2019-09-17 20:35:48 -0300

    J wa(15%)

--- J.cpp
+++ J.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 #define NMAX 14
 
@@ -12,9 +13,10 @@ struct player{
 
     int passarCarta(){
         int q=10, c;
-        if(wildcard){
-            for(int i=13; i>=1; i++){
-                if(cards[i] <= q){
+        //cout << "passei " << endl;
+        if(wildcard || cards[0] == 0){
+            for(int i=13; i>=1; i--){
+                if(cards[i] <= q && cards[i] > 0){
                     q = cards[i]; c = i;
                 }
             }
@@ -22,12 +24,14 @@ struct player{
             this->wildcard = false;
             return c;
         }
-        else{
+        else if(cards[0]> 0){
+            cards[0]--;
             return 0;
         }
     }
 
     void receberCarta(int c){
+        //cout << "recebi " << c << endl;
         cards[c]++;
         if(c == 0){
             this->wildcard = true;
@@ -40,12 +44,55 @@ struct player{
             if(cards[i] == 4){
                 return true;
             }
+            if(cards[i] > 0 && cards[i] < 4){
+                return false;
+            }
         }
+        return false;
     }
+
+    void printCards(){
+        for(int i=0; i<14; i++){
+            cout << cards[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+player Jogador[NMAX];
+map<char, int> card_map;
+
+void initMap(){
+    card_map['W'] = 0; card_map['1'] = 1; card_map['2'] = 2; card_map['3'] = 3; 
+    card_map['4'] = 4; card_map['5'] = 5; card_map['6'] = 6; card_map['7'] = 7; 
+    card_map['8'] = 8; card_map['9'] = 9; card_map['D'] = 10; card_map['Q'] = 11; 
+    card_map['J'] = 12; card_map['K'] = 13;
 }
 
-char card_map[14] = {'W', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'D', 'J', 'Q', 'K'};
-
 int main(){
-
+    int n, k, winner, atual, prox;
+    char c;
+    initMap();
+    cin >> n >> k;
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<4; j++){
+            cin >> c;
+            Jogador[i].receberCarta(card_map[c]);
+        }
+    }
+    Jogador[k].receberCarta(0);
+    atual = k; prox = (k%n)+1;
+    while(true){
+        Jogador[prox].receberCarta(Jogador[atual].passarCarta());
+        /*for(int i=1; i<=n; i++){
+            Jogador[i].printCards();
+        }
+        cout << endl << endl;*/
+        if(Jogador[atual].vencer()){
+            winner = atual;
+            break;
+        }
+        atual = prox; prox = (atual%n)+1;
+    }
+    cout << winner << endl;
 }
