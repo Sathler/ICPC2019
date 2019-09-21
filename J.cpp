4b4574308939a0ac07603770d8decb6225d9d74c
Sathler <Vinicius.sathler3@yahoo.com.br>
2019-09-21 15:23:29 -0300

    Resolucao J

--- J.cpp
+++ J.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <map>
 
-#define NMAX 14
+#define NMAX 15
 
 using namespace std;
 
@@ -12,8 +12,7 @@ struct player{
     bool wildcard;
 
     int passarCarta(){
-        int q=10, c;
-        //cout << "passei " << endl;
+        int q=1000, c;
         if(wildcard || cards[0] == 0){
             for(int i=13; i>=1; i--){
                 if(cards[i] <= q && cards[i] > 0){
@@ -24,46 +23,39 @@ struct player{
             this->wildcard = false;
             return c;
         }
-        else if(cards[0]> 0){
+        else{
             cards[0]--;
             return 0;
         }
     }
 
     void receberCarta(int c){
-        //cout << "recebi " << c << endl;
         cards[c]++;
         if(c == 0){
             this->wildcard = true;
         }
     }
 
+    //verifica se o jogador venceu
     bool vencer(){
-        bool cond;
-        for(int i=1; i<=13; i++){
+        for(int i=0; i<=13; i++){
             if(cards[i] == 4){
                 return true;
             }
             if(cards[i] > 0 && cards[i] < 4){
-                return false;
+                 return false;
             }
         }
         return false;
-    }
-
-    void printCards(){
-        for(int i=0; i<14; i++){
-            cout << cards[i] << " ";
-        }
-        cout << endl;
     }
 };
 
 player Jogador[NMAX];
 map<char, int> card_map;
 
+//Map para converter a carta lida para seu valor numerico, wildcard=0
 void initMap(){
-    card_map['W'] = 0; card_map['1'] = 1; card_map['2'] = 2; card_map['3'] = 3; 
+    card_map['W'] = 0; card_map['A'] = 1; card_map['2'] = 2; card_map['3'] = 3; 
     card_map['4'] = 4; card_map['5'] = 5; card_map['6'] = 6; card_map['7'] = 7; 
     card_map['8'] = 8; card_map['9'] = 9; card_map['D'] = 10; card_map['Q'] = 11; 
     card_map['J'] = 12; card_map['K'] = 13;
@@ -72,7 +64,7 @@ void initMap(){
 int main(){
     int n, k, winner, atual, prox;
     char c;
-    initMap();
+    initMap(); //inicializa o map
     cin >> n >> k;
     for(int i=1; i<=n; i++){
         for(int j=0; j<4; j++){
@@ -80,19 +72,24 @@ int main(){
             Jogador[i].receberCarta(card_map[c]);
         }
     }
-    Jogador[k].receberCarta(0);
-    atual = k; prox = (k%n)+1;
+    Jogador[k].receberCarta(0); //entrega a wildcard para o jogador inicial
+    atual = k; prox = (k%n)+1; 
+
+    //verifica se alguem vence ja na primeira rodada <<IMPORTANTE>>
+    for(int i=1; i<=n; i++){ 
+        if(Jogador[i].vencer()){
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    //inicio da simulacao
     while(true){
         Jogador[prox].receberCarta(Jogador[atual].passarCarta());
-        /*for(int i=1; i<=n; i++){
-            Jogador[i].printCards();
-        }
-        cout << endl << endl;*/
         if(Jogador[atual].vencer()){
-            winner = atual;
-            break;
+            cout << atual << endl;
+            return 0;
         }
         atual = prox; prox = (atual%n)+1;
     }
-    cout << winner << endl;
 }
